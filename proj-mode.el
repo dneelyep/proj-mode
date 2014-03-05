e22cc9690002187d283d4c7f938007cb1b478699
Daniel Neel <dneelyep@gmail.com>
2014-03-05 12:14:13 -0500

    Got the code functional for my purposes. Can now create new projects in specified folders.

--- proj-mode.el
+++ proj-mode.el
@@ -1,20 +1,26 @@
-;; So what we need to do is:
-;; * Have a proj-mode-setup function that sets the template file, and takes/stores a default project directory.
-;; * Have a proj-mode-create-project function that takes in a directory and makes a new project.
 (defun proj-mode-setup ()
   "Retrieve initial parameters required for setting up proj-mode."
   (interactive)
-  (prompt-for-proj-directory)
-  (prompt-for-proj-template-file))
+  (proj-prompt-for-proj-directory)
+  (proj-prompt-for-proj-template-file))
 
-(defun prompt-for-proj-directory ()
+(defun proj-prompt-for-proj-directory ()
   "Prompt the user to enter the directory which will initially be used when creating a new project."
   (setq directory (read-file-name "Set default project directory:")))
 
-(defun prompt-for-proj-template-file ()
+(defun proj-prompt-for-proj-template-file ()
   "Prompt the user to enter the template file which will be automatically placed in new projects."
   (setq template-file (read-file-name "Set project template file:")))
 
 (defun proj-create-project ()
   "Create a new project with a specified name in the default project directory."
-  (setq project-name (read-string "Project name:")))
+  (interactive)
+  (setq project-name (read-string "Project name:"))
+  (setq project-directory (concat directory  project-name))
+  (make-directory project-directory)
+  (copy-file template-file (concat project-directory "/notes.org"))
+  
+  ;; Open the default notes file for the new project.
+  (find-file (concat project-directory "/notes.org")))
+
+;; TODO I shouldn't be hard-coding the name of the template file to notes.org...
