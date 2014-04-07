48a14939cf85c22ccf38f120af7e3916936997df
Daniel Neel <dneelyep@gmail.com>
2014-04-07 17:37:14 -0400

    Fixed a couple bugs related to adding customize features.

--- proj-mode.el
+++ proj-mode.el
@@ -15,25 +15,25 @@
 
 (defun proj-prompt-for-proj-directory ()
   "Prompt the user to enter the directory which will initially be used when creating a new project."
-  (customize-set-variable proj-mode-project-directory
+  (customize-set-variable 'proj-mode-project-directory
 			  (read-file-name "Set default project directory:")))
 
 (defun proj-prompt-for-proj-template-file ()
   "Prompt the user to enter the template file which will be automatically placed in new projects."
-  (customize-set-variable proj-mode-template-file
+  (customize-set-variable 'proj-mode-template-file
 			  (read-file-name "Set project template file:")))
 
 (defun proj-create-project ()
   "Create a new project with a specified name in the default project directory."
   (interactive)
   (setq project-name (read-string "Project name:"))
-  (setq project-directory (concat directory  project-name))
-  (make-directory project-directory)
-  (copy-file template-file (concat project-directory
+  (setq proj-mode-project-directory (concat proj-mode-project-directory project-name))
+  (make-directory proj-mode-project-directory)
+  (copy-file proj-mode-template-file (concat proj-mode-project-directory
 				   "/"
-                                   (file-name-nondirectory template-file)))
+                                   (file-name-nondirectory proj-mode-template-file)))
   
   ;; Open the default notes file for the new project.
-  (find-file (concat project-directory
+  (find-file (concat proj-mode-project-directory
 		     "/"
-		     (file-name-nondirectory template-file))))
+		     (file-name-nondirectory proj-mode-template-file))))
